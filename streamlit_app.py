cbad4543ecdd3247a0dbf42cd09ee2109c1221c6
ndamukunda139 <37266165+ndamukunda139@users.noreply.github.com>
2025-05-07 12:30:17 +0000

    Minor change

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,4 @@ st.title("July Intake Model Deployment")
 #Add header
 st.header("Instructor: Joseph Gitau")
 st.text("Hello All, My name is Joseph Gitau, and I will be taking you through this course within few days")
+st.markdown("Welcome to the course, hope you will enjoy")
